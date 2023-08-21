645fe06259ead57348641a8e37e3003e6dd3ab33
CODE WITH JOSE DAVID <48775594+JoseCode-CO@users.noreply.github.com>
2023-08-20 21:58:13 -0500

    Creando componente html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, LasName } from "./Greeting";
+import Button from "./Button";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
@@ -8,5 +9,7 @@ root.render(
     <>
         <Greeting title="Parametro 1" />
         <LasName name="Lora" car="Mazda"/>
+        <Button text="BACK"/>
+        <Button text="OK"/>
     </>
 )
