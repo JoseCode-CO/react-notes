8d9b01734bd31ef13ce857d8197dd16f796589b9
CODE WITH JOSE DAVID <48775594+JoseCode-CO@users.noreply.github.com>
2023-08-20 22:15:24 -0500

    Event

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import Button from "./Button";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+const  handleChange = (e) =>{
+    console.log(e.target.value)
+}
 root.render(
     <>
         <Greeting title="Parametro 1" />
         <LasName name="Lora" car="Mazda"/>
         <Button text="BACK"/>
         <Button text="OK"/>
+        <input id="hola" onChange={handleChange}/>
     </>
 )
